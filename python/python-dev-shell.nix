3072b3be502d2ac47748f81691f39cf760bd3ae3
nufianqodar <77nurfianqodar@gmail.com>
2024-12-07 11:33:15 +0700

    mod

--- python/python-dev-shell.nix
+++ python/python-dev-shell.nix
@@ -11,14 +11,12 @@ mkShell {
 
 
   # === Environment Variables
-  # Fix libc++.so.6 error
+  
+  # fix libc++.so.6 error
   NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
     stdenv.cc.cc
   ];
-  GOOGLE_APPLICATION_CREDENTIALS=/home/fynn/Projects/Capstone/rest-api-fastapi/service-account.json;
   NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
-
-
 
   # === Hook
   shellHook = ''
