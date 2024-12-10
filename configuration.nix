c84b6f9f8589535e941ce050d88b70f44b065e7e
nufianqodar <77nurfianqodar@gmail.com>
2024-12-11 00:52:51 +0700

    refactor

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
 
@@ -10,146 +7,215 @@
       ./hardware-configuration.nix
     ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # Boot
+  boot = {
+    # Bootloader
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    }
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Asia/Jakarta";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+  # Nix
+  nix = {
+    # Settings
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+    };
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  # Networking
+  networking = {
+    hostName = "lucille";
+    networkmanager = {
+      enable = true;
+    };
+  };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.fynn = {
-    isNormalUser = true;
-    description = "Nurfian Qodar";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
-    packages = with pkgs; [
-    #  thunderbird
+  # Time
+  time = {
+    timeZone = "Asia/Jakarta";
+  };
+
+  # i18n
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  # Services
+  services = {
+    xserver = {
+      enable = true;
+      displayManager = {
+        gdm.enable = true;
+      };
+
+      desktopManager = {
+        gnome.enable = true;
+      };
+
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+    };
+
+    printing = {
+      enable = true;
+    };
+
+    mysql = {
+       enable = true;
+       package = pkgs.mariadb;
+       dataDir = "/var/lib/mysql";
+    };
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+
+    };
+
+    openssh = {
+      enable = true;
+    };
+  };
+
+  virtualisation = {
+    docker = {
+      enable = true;
+    };
+  };
+  
+  # Hardware
+  hardware = {
+    pulseaudio.enable = false;
+  };
+
+  # Security
+  security = {
+    rtkit = {
+      enable = true;
+    };
+  };
+
+  # Users
+  users = {
+    users = {
+      
+      fynn = {
+        isNormalUser = true;
+        description = "Nurfian Qodar";
+        extraGroups = [ "networkmanager" "wheel" "docker" ];
+        packages = with pkgs; [
+          #  thunderbird
+        ];
+      };
+    };
+  };
+
+  # Programs
+  programs = {
+    firefox = {
+      enable = true;
+    };
+
+    git = {
+      enable = true;
+      userName = "nurfianqodar";
+      userEmail = "77nurfianqodar@gmail.com";
+      extraConfig = {
+        init = {
+          defaultBranch = "main";
+        };
+      };
+      aliases = {
+        "grap" = "log --oneline --graph --all --oneline";
+      };
+    };
+  };
+
+  # Nixpkgs
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+  # Environment
+  environment = {
+    systemPackages = with pkgs; [
+      # A
+      aircrack-ng
+      autodock-vina
+      # B
+      # C
+      cmake
+      # D
+      sqlitebrowser
+      direnv
+      discord
+      docker
+      # E
+      extra-cmake-modules
+      # F
+      # G
+      gcc
+      gh
+      git
+      glibc
+      gnome-tweaks
+      gnumake
+      go
+      google-chrome
+      gparted
+      gromacs
+      # H
+      # I
+      # J
+      # K
+      # L
+      libcxx
+      libgcc
+      libreoffice
+      # M
+      mariadb
+      # N
+      neofetch
+      neovim
+      # P
+      pipenv
+      poetry
+      postgresql
+      postman
+      pymol
+      python3
+      # R
+      R
+      rustup
+      # S
+      spotify
+      stdenv.cc.cc.lib
+      # T
+      tor
+      tor-browser
+      tshark
+      # V
+      vlc
+      vscode
+      # W
+      wget
+      wireshark
     ];
   };
 
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-     # A
-     # B
-     # C
-     cmake
-     # D
-     direnv
-     discord
-     docker
-     # E
-     extra-cmake-modules
-     # G
-     gcc
-     gh			# Github CLI tools
-     git		# Git VCS
-     glibc
-     gnome-tweaks
-     gnumake
-     go			# Go programming language
-
-     #L
-     libcxx
-     libgcc
-     # N
-     neofetch
-     neovim		# Neovim
-     # P
-     pipenv		# Python virtual environtment
-     poetry		# Python package manager
-     python3		# Python3 programming language
-     # R
-     rustup		# Rust toolchain manager
-     # S
-     spotify
-     stdenv.cc.cc.lib
-     # V
-     vlc
-     vscode		# MS VSCode
-     # W
-     wget		# wget
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
-
+  system = {
+    stateVersion = "24.11";
+  };
 }
